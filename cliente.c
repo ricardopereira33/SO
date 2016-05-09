f32539cb226925e69464792a0f1fff08f676870b
Ricardo Pereira <ricardo.agp.914@gmail.com>
2016-05-09 20:41:17 +0100

    servidor envia sinais ao cliente

--- cliente.c
+++ cliente.c
@@ -7,23 +7,36 @@
 #include <unistd.h>
 
 
+void my_alarm(){
+    printf("Acabou\n");
+}
+
 int main(int argc, char** argv) {
     int i,sum=0,n;
     char* cat;
+    char buffer[128];
     int pid_pipe = open("/Users/Ricardo/Desktop/clientes", O_WRONLY);
 
     for(i=1;argv[i]!=NULL;i++){
     	sum+=strlen(argv[i]);
     }
 
-    cat = malloc (sum*sizeof(char));
+    cat = malloc ((sum+strlen(buffer))*sizeof(char));
 
     for(i=1;argv[i]!=NULL;i++){
     	strcat(cat,argv[i]);
     	strcat(cat,"\n");
     }
+
+    sprintf(buffer, "%d\n", getpid());
+
+    strcat(cat,buffer);
     
     write(pid_pipe, cat, strlen(cat));
+    
+    signal(SIGALRM,my_alarm);
+    
+    pause();
 
    	free(cat);
     close(pid_pipe);
--- servidor.c
+++ servidor.c
@@ -5,21 +5,23 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
 
-char** readln(char *buf){
+char** readln(char *buf,int *n){
     char** buff=malloc(10*sizeof(char*));
     int i=0;
     char* token;
     token = strtok(buf, "\n");
    
-    /* walk through other tokens */
     while( token != NULL ) {
       buff[i]=malloc(strlen(token)*sizeof(char));
       strcpy(buff[i],token);
+      buff[i][strlen(token)]=0;
       i++;
       token = strtok(NULL, "\n");
    }
     buff[i]=NULL;
+    *n=i-1;
     return buff;
 }
 
@@ -30,7 +32,7 @@ int main(int argc, char** argv) {
     	char buf[128];
         char bufcopy[128];
         char** buf2;
-    	int n,sair=1;
+    	int n,sair=1,i;
     	int pid_pipe ;
     	int pid_log = open("/Users/Ricardo/Desktop/log.txt",O_WRONLY | O_CREAT | O_TRUNC, 0666);
         
@@ -39,14 +41,29 @@ int main(int argc, char** argv) {
     	    while ( (n = read(pid_pipe, buf, 128))>0 )  {
                 buf[n]=0;
                 strcpy(bufcopy,buf);
-                buf2=readln(bufcopy);
+                buf2=readln(bufcopy,&i);
 
-                if(strcmp(buf,"exit\n")==0){
+                if(strcmp(buf2[0],"exit")==0){
                     sair=0;
+                    kill(atoi(buf2[i]),SIGALRM);
                     break;
                 }
+
+                if(strcmp(buf2[0],"backup")==0){
+                    printf("backup\n");
+                    kill(atoi(buf2[i]),SIGALRM);
+                    /*mandar sinal*/
+                }
+
+                if(strcmp(buf2[0],"restore")==0){
+                    printf("restore\n");
+                    kill(atoi(buf2[i]),SIGALRM);
+                    /*mandar sinal*/
+                }
+
                 //execvp(buf2[0],buf2);
-                write(pid_log, buf, n);
+                //write(pid_log, buf, n);
+
            }
            close(pid_pipe);
         }   
